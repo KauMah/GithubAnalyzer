710dc5ecd502230ccd83084830ada995a2293372
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2023-03-25 19:55:41 -0400

    implement paginated api calls and setup for further operations

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use std::fs;
 use std::io;
 use tokio::{self};
 
-fn make_request(token: &str, username: &str) -> Result<RequestBuilder, Box<dyn Error>> {
-    let api_url = format!("https://api.github.com/users/{username}/repos?per_page=40&page=1");
+fn make_request(token: &str, username: &str, page: i16) -> Result<RequestBuilder, Box<dyn Error>> {
+    let api_url = format!(
+        "https://api.github.com/users/{username}/repos?per_page=5&page={}",
+        page
+    );
     let mut headers = HeaderMap::new();
     headers.insert(
         ACCEPT,
@@ -27,6 +30,22 @@ fn make_request(token: &str, username: &str) -> Result<RequestBuilder, Box<dyn E
     return Ok(req);
 }
 
+async fn get_git_urls(rb: RequestBuilder) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut urls = Vec::new();
+    let res = rb.send().await?.text().await?;
+    let json: Vec<Value> = serde_json::from_str(&res).expect("Failed to convert response to JSON");
+    for repo in json.iter() {
+        let url = repo
+            .get("git_url")
+            .expect("Failed to get 'git_url' from JSON object")
+            .to_string();
+        // println!("{}", &url[7..url.len() - 1]);
+        urls.push(url[7..url.len() - 1].to_string());
+    }
+
+    Ok(urls)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     println!("Initializing - Github Analyzer...");
@@ -42,18 +61,35 @@ async fn main() -> Result<(), reqwest::Error> {
     println!("{}[2J", 27 as char);
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     // end clear terminal
-    let req = make_request(&token, &username).expect("Something went wrong building the URL");
 
-    let res = req.send().await?.text().await?;
-    let js: Value = serde_json::from_str(&res).expect("This should just work");
-    let pretty = serde_json::to_string_pretty(&js).expect("This should just work");
+    let mut git_urls: Vec<String> = Vec::new();
+    let mut page: i16 = 1;
+    loop {
+        let req =
+            make_request(&token, &username, page).expect("Something went wrong building the URL");
+        let new_urls = get_git_urls(req).await.expect("msg");
+        if new_urls.len() == 0 {
+            break;
+        }
+        for url in new_urls.iter() {
+            println!("{}", url);
+        }
+        git_urls.extend(new_urls);
+        page = page + 1;
+        println!();
+    }
+
+    // let res = req.send().await?.text().await?;
+    // let js: Value = serde_json::from_str(&res).expect("This should just work");
+    // let pretty = serde_json::to_string_pretty(&js).expect("This should just work");
+
     // write to file for a lil test
     // println!("{:#?}", req);
     // let mut file = File::create("out.txt").await.expect("Please work lol");
     // file.write_all(pretty.as_bytes())
     //     .await
     //     .expect("Failed to write to file");
-    println!("{}", &pretty);
+    // println!("{}", &pretty);
 
     Ok(())
 }
